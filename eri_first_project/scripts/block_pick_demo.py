12d078b6a0b38a3263560a99d7d79213be40f905
Eri Sawada <esawada2003@gmail.com>
2025-04-15 16:58:11 +0900

    get depths from multiple points

--- eri_first_project/scripts/block_pick_demo.py
+++ eri_first_project/scripts/block_pick_demo.py
@@ -13,6 +13,7 @@ from cv_bridge import CvBridge, CvBridgeError
 from aerial_robot_msgs.msg import FlightNav
 from message_filters import TimeSynchronizer, Subscriber
 # import cv2
+import numpy as np
 
 
 class BlockPickDemo():
@@ -20,7 +21,7 @@ class BlockPickDemo():
         self.ri = RobotInterface()
         rospy.sleep(1.0) # wait for joint updated
         self.bridge = CvBridge()
-        
+
         # rospy.Subscriber("/rs_d435/color/image_rect_color", Image, self.cb_image)
         # rospy.Subscriber("/rs_d435/aligned_depth_to_color/image_raw", Image, self.cb_depth)
         # rospy.Subscriber("/camera/color/image_raw", Image, self.cb_image)
@@ -55,23 +56,53 @@ class BlockPickDemo():
             max_contour = max(contours, key=cv2.contourArea)
 
             M = cv2.moments(max_contour)
-            if M["m00"] != 0:
-                cx = int(M["m10"] / M["m00"])
-                cy = int(M["m01"] / M["m00"])
-                self.height, self.width = mask.shape
-                self.cx = cx
-                self.cy = cy
-                print(f"最大輪郭の重心座標: ({cx}, {cy})")
+
+            if M["m00"] < 10: # this might needs to be tuned
+                return
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+            image_height, image_width = mask.shape
+            self.cx = cx
+            self.cy = cy
+            print(f"最大輪郭の重心座標: ({cx}, {cy})")
+            
+            rand_offsets = (np.random.rand(100, 2) - 0.5) * 100  # this might needs to be tuned
+            candidate_points = np.array([self.cx, self.cy]) + rand_offsets
+            candidate_points = candidate_points.astype(int)
+
+            valid_mask = (
+                (0 <= candidate_points[:, 0]) & (candidate_points[:, 0] < image_width) &
+                (0 <= candidate_points[:, 1]) & (candidate_points[:, 1] < image_height)
+            )
+            candidate_points = candidate_points[valid_mask]
+
+            ys = candidate_points[:, 1]
+            xs = candidate_points[:, 0]
+            mask_values = mask[ys, xs]
+
+            valid_mask_points = mask_values > 0
+            points_x = xs[valid_mask_points]
+            points_y = ys[valid_mask_points]
+
+            if len(points_x) == 0:
+                return
+
         except CvBridgeError as e:
             print(e)
+            return
 
         try:
             depth_image = self.bridge.imgmsg_to_cv2(depth, desired_encoding='passthrough')
             scale = 0.001 # this might not be true
             height, width = depth_image.shape
-            cx = (self.cx * width) // self.width
-            cy = (self.cy * height) // self.height
-            self.depth =  depth_image[cy, cx] * scale
+            points_x = (points_x * width) // image_width
+            points_y = (points_y * height) // image_height
+
+            depths = []
+            for i in range(len(points_x)):
+                if points_x[i] < width and points_y[i] < height:
+                    depths.append(depth_image[points_y[i], points_x[i]] * scale)
+            self.depth = np.mean(depths)
             self.update_time_depth = depth.header.stamp
             print(f"depth: {self.depth}")
         except Exception as e:
@@ -87,13 +118,14 @@ class BlockPickDemo():
             # self.ri.setJointAngle(["joint1", "joint3"], [1.0, 1.0])
             if hasattr(self, "update_time_depth"):
                 if (rospy.Time.now() - self.update_time_depth).to_sec() < 1.0:
-                    nav_msg = FlightNav()
-                    nav_msg.control_frame = FlightNav.LOCAL_FRAME
-                    nav_msg.target = FlightNav.COG
-                    nav_msg.pos_xy_nav_mode = FlightNav.VEL_MODE
-                    nav_msg.target_vel_x = -0.1
-                    nav_msg.target_vel_y = 0.1
-                    self.nav_pub.publish(nav_msg)
+                    if self.depth > 0.00001: # this might needs to be tuned
+                        nav_msg = FlightNav()
+                        nav_msg.control_frame = FlightNav.LOCAL_FRAME
+                        nav_msg.target = FlightNav.COG
+                        nav_msg.pos_xy_nav_mode = FlightNav.VEL_MODE
+                        nav_msg.target_vel_x = -0.1
+                        nav_msg.target_vel_y = 0.1
+                        self.nav_pub.publish(nav_msg)
 
             # user code end
 
