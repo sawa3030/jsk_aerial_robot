584b9741bb318d72fd4b8a0cf66f2fb88ec7073b
Eri Sawada <esawada2003@gmail.com>
2026-05-23 17:58:27 +0900

    remove unnecessary files

--- robots/uuv_d/bin/rosbag_control_data.sh
+++ robots/uuv_d/bin/rosbag_control_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosrun aerial_robot_base rosbag_control_data.sh ${1:-hydrus} ${1:-hydrus}/zed/odom ${1:-hydrus}/realsense1/odom/throttle ${1:-hydrus}/realsense1/odom ${1:-hydrus}/realsense2/odom/throttle ${1:-hydrus}/realsense2/odom ${@:2}
+rosrun aerial_robot_base rosbag_control_data.sh ${1:-uuv_d} ${1:-uuv_d}/zed/odom ${1:-uuv_d}/realsense1/odom/throttle ${1:-uuv_d}/realsense1/odom ${1:-uuv_d}/realsense2/odom/throttle ${1:-uuv_d}/realsense2/odom ${@:2}
